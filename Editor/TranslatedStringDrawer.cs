1acea97a54eb0d1eb620009aca0a7126be88b9bd
Victor Rafael <contato@victorafael.com>
2024-03-16 20:04:00 -0400

    fix rendering on TranslatedString drawer

--- Editor/TranslatedStringDrawer.cs
+++ Editor/TranslatedStringDrawer.cs
@@ -8,22 +8,17 @@ namespace com.victorafael.translation
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+            // position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
 
             EditorGUI.BeginProperty(position, label, property);
-            GUI.BeginGroup(position);
             EditorGUILayout.BeginHorizontal();
-
             SerializedProperty key = property.FindPropertyRelative("key");
-            EditorGUILayout.PropertyField(key, GUILayout.MinWidth(0));
+            EditorGUILayout.PropertyField(key, label, GUILayout.MinWidth(0));
             if (GUILayout.Button("...", GUILayout.Width(20)))
             {
                 TranslationEditorWindow.ShowWindow();
             }
             EditorGUILayout.EndHorizontal();
-            GUI.EndGroup();
-
-
 
             EditorGUI.EndProperty();
         }
